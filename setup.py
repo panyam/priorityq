d20965e4bc4254757b90376791445b5689949a54
Sriram Panyam <spanyam@linkedin.com>
2017-02-14 07:06:27 -0800

    Doc update

--- setup.py
+++ setup.py
@@ -1,70 +1,3 @@
-"""
-PriorityQ
----------
-
-PriorityQ is a library for managing a priority queue (PQ) with a cleaner API to enable custom comparators, 
-finding references to values efficiently (in constant time) and deleting values from the PQ.   This was 
-developed because the current heapq module (in python's standard library) does not provide an efficient
-find operation (it is O(n)) and has no easy way to deleting an element and ensuring the heap invariant
-afterwards.
-
-Features
-````````
-
-* O(1) finding of elements
-* Deletion of elements possible (in O(log n)).
-* Adjusting of the priority of an element without requiring a deletion followed by an insertion.
-* Opaque handles to elements that can be used to reference to the same item again.
-* Duplicate elements allowed.
-* Custom comparator function can be passed to the PQ itself instead of needing to implement __cmp__.
-
-It is simple to use
-```````````````````
-
-To create a PQ simply do:
-
-.. code:: python
-
-    # A simple object with a comparator
-    class Item(object):
-        def __init__(self, value):
-            self.value = value
-        def __cmp__(self, another):
-            return cmp(self.value, another.value)
-
-    from priorityq import PQ
-    pq = PQ()
-    pq.heapify([Item(r) for r in [1, 10, 2, 20, 4, 7, 9, 3, 5, 6]])
-
-    print list(pq)
-
-    # Should print:
-    # 1 2 3 4 5 6 7 9 10 20
-
-    handle_10 = pq.find(25)   #   Happens in O(1)
-
-    handle_10.value = 12      #   Modify its value - O(log n)
-    pq.adjust(handle_10)      #   Indicate to the heap to reprioritise/adjust it
-
-    print list(pq)
-
-    # Should print:
-    # 1 2 3 4 5 6 7 9 20 25
-
-    handle_10.value = 10      #   Modify its value using the same opaque pointer as before
-    pq.adjust(handle_10)      #   Indicate to the heap to reprioritise/adjust it
-
-    print list(pq)
-
-    # Should print:
-    # 1 2 3 4 5 6 7 9 10 20
-
-Links
-`````
-* `Documentation Wiki <https://github.com/panyam/priorityq/wiki>`_
-* `Website <https://github.com/panyam/priorityq>`_
-"""
-
 from __future__ import absolute_import
 import os
 import re
@@ -95,29 +28,30 @@ if "install" in sys.argv:
 # Any packages inside the priorityq source folder we dont want in the packages
 EXCLUDE_FROM_PACKAGES = [ ]
 
-
-with open('priorityq/__init__.py', 'rb') as f:
-    _version_re = re.compile(r'__version__\s+=\s+(.*)')
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
-
 # for scheme in INSTALL_SCHEMES.values(): scheme['data']=scheme['purelib']
 
 print "=" * 80
 print "Packages: ", find_packages(exclude=EXCLUDE_FROM_PACKAGES)
 print "=" * 80
 
+def get_version():
+    with open('priorityq/__init__.py', 'rb') as f:
+        _version_re = re.compile(r'__version__\s+=\s+(.*)')
+        return str(ast.literal_eval(_version_re.search(f.read().decode('utf-8')).group(1)))
+
+def get_description():
+    return open("README.md").read()
+
 setup(name="priorityq",
-      version=version,
+      version=get_version(),
       requires = [ ],
       description="A clean priority queue library with several types and utility helpers.",
+      long_description=get_description(),
       author="Sri Panyam",
       author_email="sri.panyam@gmail.com",
       url="https://github.com/panyam/priorityq",
       package_data = {
           'priorityq': [
-              "data/templates/backends/java/*",
-              "data/templates/transformers/java/*"
           ]
       },
       packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
